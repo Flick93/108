7dd3ee47c3112ca5dc44878d1146b0597e493168
Flick93 <p.afflick@outlook.com>
2024-09-12 16:57:38 -0700

    first commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+import "./Product.css"
+
+function Product(){
+    return(
+        <div className="product">
+            <h5>These are our Products</h5>
+            <label>$12.49</label>
+        </div>
+    );
+}
+
+export default Product;
